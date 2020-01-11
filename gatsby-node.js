7c192f3372691b82fd5f8c92b11e681474822646
MateuszPospiech <pospiechmateusz@gmail.com>
2020-01-12 00:45:49 +0100

    GatsbyJS blog auto slug post path

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,47 @@
  */
 
 // You can delete this file if you're not using it
+
+const { slugify } = require('./src/util/utilityFunctions')
+
+// exports.onCreateNode = ({ node, actions}) => {
+
+// }
+
+export.createPages =  ({ actions, graphql }) => {
+   const { createPage } = actions;
+   const singlePostTemplate = path.resolve('src/templates/single-post.js')
+
+   return qraphql(`
+      {
+         allMarkdownRemark{
+            edges{
+               node{
+                  frontmatter{
+                     author
+                     tags
+                  }
+                  fields{
+                     slug
+                  }
+               }
+            }
+         }
+      }
+   `).then(res => {
+      if(res.errors) return Promise.reject(res.errors)
+
+      const posts = res.data.allMarkdownRemark.edges
+
+      posts.forEach({node}) => {
+         createPage({
+            path: node.fields.slug,
+            component: singlePostTemplate,
+            context: {
+               slug: node.fields.slug
+            }
+         })
+      }
+
+   })
+}
