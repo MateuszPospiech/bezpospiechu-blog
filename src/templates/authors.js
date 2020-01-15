3049cda292386077141ea4a2526228fa60133bf6
MateuszPospiech <pospiechmateusz@gmail.com>
2020-01-15 20:55:08 +0100

    GatsbyJS blog added author photo and array of informations

---
+++ src/templates/authors.js
@@ -0,0 +1,20 @@
+const authors = [
+   {
+      name: 'Mateusz Pośpiech',
+      imageUrl: 'mateusz.jpg',
+      bio: 'Programista frontend z 3 letnim doświadczeniem komercyjnym. Fan sportu oraz podróży.'
+      facebook: 'https://www.facebook.com',
+      instagram: 'https://www.instagram.com',
+      linkedin: 'https://www.linkedin.com',
+   },
+   {
+      name: 'Angelika Mikiciuk',
+      imageUrl: 'mateusz.jpg',
+      bio: 'Tester oprogramowania z 3 letnim doświadczeniem komercyjnym. Fan sportu oraz podróży.'
+      facebook: 'https://www.facebook.com',
+      instagram: 'https://www.instagram.com',
+      linkedin: 'https://www.linkedin.com',
+   }
+]
+
+module.exports = authors
